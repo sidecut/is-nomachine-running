abd9b5feb9534a675ffbfd62fb9d6c5e928a398b
Jim Raden <radenj@ccf.org>
2020-06-11 14:55:48 -0400

    Displays status

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module is_nomachine_running
 
 go 1.14
 
-require github.com/labstack/echo/v4 v4.1.16
+require (
+	github.com/labstack/echo/v4 v4.1.16
+	github.com/mitchellh/go-ps v1.0.0
+)
--- server.go
+++ server.go
@@ -1,16 +1,61 @@
 package main
 
 import (
+	"errors"
 	"html/template"
 	"io"
 	"net/http"
+	"os"
 
 	"github.com/labstack/echo/v4"
+	"github.com/mitchellh/go-ps"
 )
 
 // Hello just outputs "hello, world" using an HTML template
 func Hello(c echo.Context) error {
 	return c.Render(http.StatusOK, "hello", "World")
+}
+
+type nomachineStatus struct {
+	HostName         string
+	NoMachineRunning bool
+	ClientAttached   bool
+}
+
+func getFirstProcessByName(name string) (int, error) {
+	processes, err := ps.Processes()
+	if err != nil {
+		return -1, err
+	}
+
+	for _, process := range processes {
+		if process.Executable() == name {
+			return process.Pid(), nil
+		}
+	}
+
+	return -1, errors.New("Could not find process " + name)
+}
+
+// Index writes the status of NoMachine
+func Index(c echo.Context) error {
+	hostName, err := os.Hostname()
+	if err != nil {
+		c.Logger().Fatal(err)
+	}
+	status := nomachineStatus{HostName: hostName}
+
+	noMachinePid, _ := getFirstProcessByName("nxserver.bin")
+	noMachineClientPid, _ := getFirstProcessByName("nxexec")
+
+	if noMachinePid >= 0 {
+		status.NoMachineRunning = true
+	}
+	if noMachineClientPid >= 0 {
+		status.ClientAttached = true
+	}
+
+	return c.Render(http.StatusOK, "index", status)
 }
 
 func main() {
@@ -21,6 +66,7 @@ func main() {
 	e := echo.New()
 	e.Renderer = t
 	e.GET("/hello", Hello)
+	e.GET("/", Index)
 
 	// e.GET("/", func(c echo.Context) error {
 	// 	return c.String(http.StatusOK, "Hello, World!")
