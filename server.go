73c38cbcea2e9c950d8481a953770056f4d1398d
James Raden <james.raden@gmail.com>
2020-06-14 15:07:53 -0400

    Use port 80

--- server.go
+++ server.go
@@ -49,7 +49,7 @@ func main() {
 	// e.GET("/", func(c echo.Context) error {
 	// 	return c.String(http.StatusOK, "Hello, World!")
 	// })
-	e.Logger.Fatal(e.Start(":1323"))
+	e.Logger.Fatal(e.Start(":80"))
 }
 
 // Template struct
