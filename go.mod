1445e795b07f21ceef927ed73cc7b72e06ad4981
James Raden <james.raden@gmail.com>
2022-03-14 23:53:24 -0400

    Convert to Fiber

--- go.mod
+++ go.mod
@@ -2,4 +2,17 @@ module github.com/sidecut/is-nomachine-running
 
 go 1.17
 
-require github.com/mitchellh/go-ps v1.0.0
+require (
+	github.com/gofiber/fiber/v2 v2.29.0
+	github.com/gofiber/template v1.6.25
+	github.com/mitchellh/go-ps v1.0.0
+)
+
+require (
+	github.com/andybalholm/brotli v1.0.4 // indirect
+	github.com/klauspost/compress v1.15.0 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.34.0 // indirect
+	github.com/valyala/tcplisten v1.0.0 // indirect
+	golang.org/x/sys v0.0.0-20220227234510-4e6760a101f9 // indirect
+)
--- server.go
+++ server.go
@@ -1,56 +1,39 @@
 package main
 
 import (
-	"html/template"
-	"io"
-	"net/http"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/compress"
+	"github.com/gofiber/template/html"
 )
 
 // Hello just outputs "Hello, world" using an HTML template
-func Hello(c echo.Context) error {
-	return c.Render(http.StatusOK, "hello", "World")
+func Hello(c *fiber.Ctx) error {
+	return c.Render("hello", fiber.Map{"Hello": "World"})
 }
 
-func statusAPI(c echo.Context) error {
+func statusAPI(c *fiber.Ctx) error {
 	status, err := getStatus()
 	if err != nil {
 		// TODO: log this error
 		return err
 	}
-	return c.JSON(http.StatusOK, status)
+	return c.Format(status)
 }
 
 func main() {
-	t := &Template{
-		templates: template.Must(template.ParseGlob("views/*.html")),
-	}
+	engine := html.New("./views", ".html")
+	app := fiber.New(fiber.Config{Views: engine})
+	app.Use(compress.New())
 
-	e := echo.New()
-	e.Renderer = t
-	e.Use(middleware.Gzip())
+	// corsConfig := middleware.CORSConfig{AllowOrigins: []string{"*"}}
+	// app.Use(middleware.CORSWithConfig(corsConfig))
 
-	corsConfig := middleware.CORSConfig{AllowOrigins: []string{"*"}}
-	e.Use(middleware.CORSWithConfig(corsConfig))
-
-	e.GET("/hello", Hello)
-	e.GET("/api", statusAPI)
-	e.Static("/", "dist")
-
-	// Start port 443
-	go func(c *echo.Echo) {
-		e.Logger.Fatal(e.StartAutoTLS(":443"))
-	}(e)
+	app.Get("/hello", Hello)
+	app.Get("/api", statusAPI)
+	app.Static("/", "dist")
 
 	// Start port 80
-	e.Logger.Fatal(e.Start(":80"))
-}
-
-// Template struct
-type Template struct {
-	templates *template.Template
-}
-
-// Render is used by Echo handler funcs
-func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
-	return t.templates.ExecuteTemplate(w, name, data)
+	log.Fatal(app.Listen(":80"))
 }
