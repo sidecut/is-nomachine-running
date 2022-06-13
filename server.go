7ba45eafcf2b8c1a2b3b6458f5e78f3ab5495787
James Raden <james.raden@gmail.com>
2022-06-13 19:21:15 -0400

    Revert "Merge pull request #29 from sidecut/feature/Remove-template"
    This reverts commit 121b3f4a9cc9704a28ffad94d6cfde6c8fd012c1, reversing
    changes made to d285600c213dd3a09d8b9dcb021c30652e4e2439.

--- server.go
+++ server.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"io"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
@@ -14,6 +16,11 @@ func init() {
 	viper.SetDefault("sslport", 443)
 }
 
+// Hello just outputs "Hello, world" using an HTML template
+func Hello(c echo.Context) error {
+	return c.Render(http.StatusOK, "hello", "World")
+}
+
 func statusAPI(c echo.Context) error {
 	status, err := getStatus()
 	if err != nil {
@@ -24,7 +31,12 @@ func statusAPI(c echo.Context) error {
 }
 
 func main() {
+	t := &Template{
+		templates: template.Must(template.ParseGlob("views/*.html")),
+	}
+
 	e := echo.New()
+	e.Renderer = t
 	e.Use(middleware.Gzip())
 	e.Use(middleware.Logger())
 	e.Use(middleware.RequestID())
@@ -32,6 +44,7 @@ func main() {
 	corsConfig := middleware.CORSConfig{AllowOrigins: []string{"*"}}
 	e.Use(middleware.CORSWithConfig(corsConfig))
 
+	e.GET("/hello", Hello)
 	e.GET("/api", statusAPI)
 	e.Static("/", "dist")
 
@@ -47,4 +60,14 @@ func main() {
 
 	// Start port 80
 	e.Logger.Fatal(e.Start(fmt.Sprintf(":%v", port)))
+}
+
+// Template struct
+type Template struct {
+	templates *template.Template
+}
+
+// Render is used by Echo handler funcs
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
 }
