6b05f94d1b2831fbbfcdcf8081f3e4d8dff34b9d
James Raden <james.raden@gmail.com>
2023-01-27 01:37:57 -0500

    Implemented as per tutorial. Not sure if it works

--- server.go
+++ server.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"net"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/spf13/viper"
+	"google.golang.org/grpc"
+
+	status "github.com/sidecut/is-nomachine-running/gen/proto/go/nomachine_status/v1"
 )
 
 func init() {
@@ -40,6 +46,21 @@ func main() {
 	port := viper.GetInt("port")
 	sslport := viper.GetInt("sslport")
 
+	go func(c *echo.Echo) {
+		listenOn := "127.0.0.1:8080"
+		listener, err := net.Listen("tcp", listenOn)
+		if err != nil {
+			e.Logger.Fatal(fmt.Errorf("failed to listen on %s: %w", listenOn, err))
+		}
+
+		server := grpc.NewServer()
+		status.RegisterGetStatusResponseServiceServer(server, &statusServiceServer{})
+		log.Println("Listening on", listenOn)
+		if err := server.Serve(listener); err != nil {
+			e.Logger.Fatal(fmt.Errorf("failed to serve gRPC server: %w", err))
+		}
+	}(e)
+
 	// Start port 443
 	go func(c *echo.Echo) {
 		e.Logger.Fatal(e.StartAutoTLS(fmt.Sprintf(":%v", sslport)))
@@ -47,4 +68,16 @@ func main() {
 
 	// Start port 80
 	e.Logger.Fatal(e.Start(fmt.Sprintf(":%v", port)))
+}
+
+type statusServiceServer struct {
+	status.UnimplementedGetStatusResponseServiceServer
+}
+
+func (s *statusServiceServer) GetStatus(ctx context.Context, req *status.GetStatusRequest) (*status.GetStatusResponse, error) {
+	return &status.GetStatusResponse{
+		HostName:         "TEST-HOST-NANE",
+		NoMachineRunning: true,
+		ClientAttached:   true,
+	}, nil
 }
