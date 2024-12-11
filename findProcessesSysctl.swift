ccc96616ed657f6a58e242482628d883db103ebd
Jim Raden <james.raden@gmail.com>
2024-12-11 18:05:43 -0500

    Get findProcessesSysctl to compile, but it fails in sysctl call

--- findProcessesSysctl.swift
+++ findProcessesSysctl.swift
@@ -3,16 +3,16 @@ import Foundation
 func findProcesses(named processName: String) -> [Int32] {
     var pids: [Int32] = []
     var taskInfo = kinfo_proc()
-    var taskInfoCount = Int32(MemoryLayout<kinfo_proc>.size)
+    var taskInfoCount = Int(MemoryLayout<kinfo_proc>.size)
     var mib: [Int32] = [CTL_KERN, KERN_PROC, KERN_PROC_ALL]
 
     let result = sysctl(&mib, UInt32(mib.count), &taskInfo, &taskInfoCount, nil, 0)
 
     if result == 0 {
-        let numberOfProcesses = taskInfoCount / Int32(MemoryLayout<kinfo_proc>.size)
+        let numberOfProcesses = taskInfoCount / Int(MemoryLayout<kinfo_proc>.size)
         let processList = UnsafeBufferPointer(start: &taskInfo, count: Int(numberOfProcesses))
 
-        for proc in processList {
+        for var proc in processList {
             let processID = proc.kp_proc.p_pid
             let processName = withUnsafePointer(to: &proc.kp_proc.p_comm) {
                 $0.withMemoryRebound(to: CChar.self, capacity: Int(MAXCOMLEN)) {
