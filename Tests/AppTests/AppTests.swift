b4fab2c071e3b05ca2d5541dc2152d95f52208e3
Jim Raden <james.raden@gmail.com>
2024-12-21 18:40:28 -0500

    Run vapor new. TODO: move everything around

---
+++ Tests/AppTests/AppTests.swift
@@ -0,0 +1,29 @@
+@testable import App
+import XCTVapor
+import Testing
+
+@Suite("App Tests")
+struct AppTests {
+    private func withApp(_ test: (Application) async throws -> ()) async throws {
+        let app = try await Application.make(.testing)
+        do {
+            try await configure(app)
+            try await test(app)
+        }
+        catch {
+            try await app.asyncShutdown()
+            throw error
+        }
+        try await app.asyncShutdown()
+    }
+    
+    @Test("Test Hello World Route")
+    func helloWorld() async throws {
+        try await withApp { app in
+            try await app.test(.GET, "hello", afterResponse: { res async in
+                #expect(res.status == .ok)
+                #expect(res.body.string == "Hello, world!")
+            })
+        }
+    }
+}
