1dd8f85cf8f4e5e56844d9790c8b3351dc6e65db
Jim Raden <james.raden@gmail.com>
2024-12-22 17:36:18 -0500

    Get rid of getFirstProcessByName

--- Sources/App/nomachinestatus.swift
+++ Sources/App/nomachinestatus.swift
@@ -7,32 +7,6 @@ struct NoMachineStatus: Content {
     var clientAttached: Bool = false
 }
 
-func getFirstProcessByName(_ name: String) -> Int32? {
-    // var processes = ProcessInfo.processInfo.processIdentifier
-    let task = Process()
-    task.launchPath = "/bin/ps"
-    task.arguments = ["-A"]
-
-    let pipe = Pipe()
-    task.standardOutput = pipe
-    task.launch()
-
-    let data = pipe.fileHandleForReading.readDataToEndOfFile()
-    if let output = String(data: data, encoding: .utf8) {
-        for line in output.components(separatedBy: "\n") {
-            if line.contains(name) {
-                let components = line.trimmingCharacters(in: .whitespaces)
-                    .components(separatedBy: " ")
-                    .filter { !$0.isEmpty }
-                if let pid = Int32(components[0]) {
-                    return pid
-                }
-            }
-        }
-    }
-    return nil
-}
-
 func getStatus() throws -> Result<NoMachineStatus, Error> {
     var status = NoMachineStatus()
 
@@ -45,11 +19,13 @@ func getStatus() throws -> Result<NoMachineStatus, Error> {
 
     status.hostName = hostName
 
-    if let noMachinePid = getFirstProcessByName("nxserver.bin"), noMachinePid > 0 {
+    let nxServerProcess = getRunningProcesses(searchForNameExact: "nxserver.bin")
+    if nxServerProcess.count > 0 {
         status.noMachineRunning = true
     }
 
-    if let noMachineClientPid = getFirstProcessByName("nxexec"), noMachineClientPid > 0 {
+    let nxExecProcess = getRunningProcesses(searchForNameExact: "nxexec")
+    if nxExecProcess.count > 0 {
         status.clientAttached = true
     }
 
@@ -61,7 +37,7 @@ struct processResult: Content {
     var name: String
 }
 
-func getRunningProcesses() -> [processResult] {
+func getRunningProcesses(searchForNameExact: String? = nil) -> [processResult] {
     var memoryInformationBase = [
         CTL_KERN,  // kernel-related parameters and information
         KERN_PROC,  // get the process id
@@ -84,7 +60,7 @@ func getRunningProcesses() -> [processResult] {
     }
 
     let entryCount = bufferSize / MemoryLayout<kinfo_proc>.stride
-    var processList: UnsafeMutablePointer<kinfo_proc>?
+    let processList: UnsafeMutablePointer<kinfo_proc>?
     processList = UnsafeMutablePointer.allocate(capacity: entryCount)
     defer { processList?.deallocate() }
 
@@ -111,8 +87,16 @@ func getRunningProcesses() -> [processResult] {
         }
         let comm = process.kp_proc.p_comm
         let name = String(cString: Mirror(reflecting: comm).children.map { $0.value as! CChar })
-        // print("PID: \(processId), App: \(name)")
-        processes.append(processResult(pid: Int(processId), name: name))
+        let processResult = processResult(pid: Int(processId), name: name)
+
+        if let searchForName = searchForNameExact {
+            // Do case insensitive search
+            if name.lowercased() == searchForName.lowercased() {
+                return [processResult]
+            }
+        }
+
+        processes.append(processResult)
     }
 
     return processes
--- Tests/AppTests/AppTests.swift
+++ Tests/AppTests/AppTests.swift
@@ -1,29 +1,31 @@
-@testable import App
-import XCTVapor
 import Testing
+import XCTVapor
+
+@testable import IsNoMachineRunning
 
 @Suite("App Tests")
 struct AppTests {
-    private func withApp(_ test: (Application) async throws -> ()) async throws {
+    private func withApp(_ test: (Application) async throws -> Void) async throws {
         let app = try await Application.make(.testing)
         do {
             try await configure(app)
             try await test(app)
-        }
-        catch {
+        } catch {
             try await app.asyncShutdown()
             throw error
         }
         try await app.asyncShutdown()
     }
-    
+
     @Test("Test Hello World Route")
     func helloWorld() async throws {
         try await withApp { app in
-            try await app.test(.GET, "hello", afterResponse: { res async in
-                #expect(res.status == .ok)
-                #expect(res.body.string == "Hello, world!")
-            })
+            try await app.test(
+                .GET, "hello",
+                afterResponse: { res async in
+                    #expect(res.status == .ok)
+                    #expect(res.body.string == "Hello, world!")
+                })
         }
     }
 }
